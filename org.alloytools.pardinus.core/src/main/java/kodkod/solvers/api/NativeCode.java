8e37efa3f2d2c46de66a8468cbe418868cdfef58
Nuno Macedo <nmacedo@fe.up.pt>
2026-04-09 15:06:54 +0100

    support for linux arm bins

--- org.alloytools.pardinus.core/src/main/java/kodkod/solvers/api/NativeCode.java
+++ org.alloytools.pardinus.core/src/main/java/kodkod/solvers/api/NativeCode.java
@@ -243,6 +243,7 @@ public class NativeCode {
 	private static Platform findPlatform() {
 		String os = System.getProperty("os.name");
 		String arch = System.getProperty("os.arch");
+		
 		for (Platform p : platforms) {
 			if (p.osarch.matcher(arch).matches() && p.osname.matcher(os).matches()) {
 				return platform = p;
